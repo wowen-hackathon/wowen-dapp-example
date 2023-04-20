569bba2e7a4ff6281c59742f0a730be44a492dec
ripa1995 <roberto.ripamonti@ymail.com>
2023-04-20 11:21:39 +0200

    feat: reverse order

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -3,77 +3,77 @@ import TweetBox from "./TweetBox";
 import Post from "./Post";
 import "./Feed.css";
 import FlipMove from "react-flip-move";
-import axios from 'axios';
-import { TwitterContractAddress } from './config.js';
-import {ethers} from 'ethers';
-import Twitter from './utils/TwitterContract.json'
+import axios from "axios";
+import { TwitterContractAddress } from "./config.js";
+import { ethers } from "ethers";
+import Twitter from "./utils/TwitterContract.json";
 
-
-function Feed({personal}) {
+function Feed({ personal }) {
   const [posts, setPosts] = useState([]);
 
   const getUpdatedTweets = (allTweets, address) => {
     let updatedTweets = [];
     // Here we set a personal flag around the tweets
-    for(let i=0; i<allTweets.length; i++) {
-      if(allTweets[i].username.toLowerCase() == address.toLowerCase()) {
+    for (let i = 0; i < allTweets.length; i++) {
+      if (allTweets[i].username.toLowerCase() == address.toLowerCase()) {
         let tweet = {
-          'id': allTweets[i].id,
-          'tweetText': allTweets[i].tweetText,
-          'isDeleted': allTweets[i].isDeleted,
-          'username': allTweets[i].username,
-          'personal': true
+          id: allTweets[i].id,
+          tweetText: allTweets[i].tweetText,
+          isDeleted: allTweets[i].isDeleted,
+          username: allTweets[i].username,
+          personal: true,
         };
         updatedTweets.push(tweet);
       } else {
         let tweet = {
-          'id': allTweets[i].id,
-          'tweetText': allTweets[i].tweetText,
-          'isDeleted': allTweets[i].isDeleted,
-          'username': allTweets[i].username,
-          'personal': false
+          id: allTweets[i].id,
+          tweetText: allTweets[i].tweetText,
+          isDeleted: allTweets[i].isDeleted,
+          username: allTweets[i].username,
+          personal: false,
         };
         updatedTweets.push(tweet);
       }
     }
+    updatedTweets = updatedTweets.reverse();
     return updatedTweets;
-  }
+  };
 
-  const getAllTweets = async() => {
+  const getAllTweets = async () => {
     try {
-      const {ethereum} = window
+      const { ethereum } = window;
 
-      if(ethereum) {
+      if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const TwitterContract = new ethers.Contract(
           TwitterContractAddress,
           Twitter.abi,
           signer
-        )
+        );
 
         let allTweets = await TwitterContract.getAllTweets();
         setPosts(getUpdatedTweets(allTweets, ethereum.selectedAddress));
       } else {
         console.log("Ethereum object doesn't exist");
       }
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getAllTweets();
   }, []);
 
-  const deleteTweet = key => async() => {
+  const deleteTweet = (key) => async () => {
     console.log(key);
 
     // Now we got the key, let's delete our tweet
     try {
-      const {ethereum} = window
+      const { ethereum } = window;
 
-      if(ethereum) {
+      if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const TwitterContract = new ethers.Contract(
@@ -88,11 +88,10 @@ function Feed({personal}) {
       } else {
         console.log("Ethereum object doesn't exist");
       }
-
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
     <div className="feed">
