6eb51e0dd94968e086489bd3047040858596e851
ripa1995 <roberto.ripamonti@ymail.com>
2023-04-20 11:14:52 +0200

    fix: remove img url

--- client/src/TweetBox.js
+++ client/src/TweetBox.js
@@ -1,46 +1,51 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./TweetBox.css";
-import Avatar from 'avataaars';
-import { generateRandomAvatarOptions } from './avatar';
+import Avatar from "avataaars";
+import { generateRandomAvatarOptions } from "./avatar";
 import { Button } from "@material-ui/core";
-import axios from 'axios';
-import { TwitterContractAddress } from './config.js';
-import {ethers} from 'ethers';
-import Twitter from './utils/TwitterContract.json'
+import axios from "axios";
+import { TwitterContractAddress } from "./config.js";
+import { ethers } from "ethers";
+import Twitter from "./utils/TwitterContract.json";
 
 function TweetBox() {
   const [tweetMessage, setTweetMessage] = useState("");
-  const [tweetImage, setTweetImage] = useState("");
   const [avatarOptions, setAvatarOptions] = useState("");
 
   const addTweet = async () => {
     let tweet = {
-      'tweetText': tweetMessage,
-      'isDeleted': false
+      tweetText:
+        tweetMessage === ""
+          ? "#WEB3 is fun with @wowen_network!"
+          : tweetMessage,
+      isDeleted: false,
     };
 
     try {
-      const {ethereum} = window
+      const { ethereum } = window;
 
-      if(ethereum) {
+      if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const TwitterContract = new ethers.Contract(
           TwitterContractAddress,
           Twitter.abi,
           signer
-        )
+        );
 
-        let twitterTx = await TwitterContract.addTweet(tweet.tweetText, tweet.isDeleted);
+        let twitterTx = await TwitterContract.addTweet(
+          tweet.tweetText,
+          tweet.isDeleted
+        );
 
         console.log(twitterTx);
       } else {
         console.log("Ethereum object doesn't exist!");
       }
-    } catch(error) {
+    } catch (error) {
       console.log("Error submitting new Tweet", error);
     }
-  }
+  };
 
   const sendTweet = (e) => {
     e.preventDefault();
@@ -48,7 +53,6 @@ function TweetBox() {
     addTweet();
 
     setTweetMessage("");
-    setTweetImage("");
   };
 
   // Similar to componentDidMount and componentDidUpdate:
@@ -62,9 +66,9 @@ function TweetBox() {
       <form>
         <div className="tweetBox__input">
           <Avatar
-            style={{ width: '100px', height: '100px' }}
-            avatarStyle='Circle'
-            {...avatarOptions }
+            style={{ width: "100px", height: "100px" }}
+            avatarStyle="Circle"
+            {...avatarOptions}
           />
           <input
             onChange={(e) => setTweetMessage(e.target.value)}
@@ -73,13 +77,6 @@ function TweetBox() {
             type="text"
           />
         </div>
-        <input
-          value={tweetImage}
-          onChange={(e) => setTweetImage(e.target.value)}
-          className="tweetBox__imageInput"
-          placeholder="Optional: Enter image URL"
-          type="text"
-        />
 
         <Button
           onClick={sendTweet}
